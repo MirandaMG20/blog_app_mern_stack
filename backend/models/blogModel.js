6a6ca22e6b767e6d922076371b12c2569401df96
MirandaMG20 <guadamiran@gmail.com>
2023-09-07 15:41:24 -0400

    User & Blog Models

---
+++ backend/models/blogModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+// Blog Entry Model
+const blogSchema = mongoose.Schema(
+    {
+         image: {
+            type: String,
+            required: [false, 'Add an image']
+        }, 
+        title: {
+            type: String,
+            required: [true, 'Please write title']
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId, // Id of the user
+            required: true,
+            ref: 'User' // Reference to the User model
+        },
+        story: {
+            type: String,
+            required: [true, 'Please write story']
+        } 
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Blog', blogSchema)
