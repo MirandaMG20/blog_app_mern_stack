d109c51452faae0ad6f598887f645cfd723de743
MirandaMG20 <guadamiran@gmail.com>
2023-09-07 15:02:24 -0400

    BlogController

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,36 +1,38 @@
+// async-handler package if you do not want to use "try" and "catch"
+const asyncHandler = require('express-async-handler')
 
 //@desc     Get Blogs 
 //@route    Get /api/blogs
 //@access   Private
-const getBlogs = async (req, res) => {
+const getBlogs = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get Blogs' })
-}
+})
 
 //@desc     Set blog
 //@route    POST /api/blogs
 //@access   Private
-const setBlog = async (req, res) => {
+const setBlog = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add text')
     }
 
     res.status(200).json({ message: 'Create Blog' })
-}
+})
 
 //@desc     Update blog
 //@route    PUT /api/blogs/:id
 //@access   Private
-const updateBlog = async (req, res) => {
+const updateBlog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Blog ${req.params.id}` })
-}
+})
 
 //@desc     Delete blog
 //@route    DELETE /api/blogs/:id
 //@access   Private
-const deleteBlog = async (req, res) => {
+const deleteBlog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete Blog ${req.params.id}` })
-}
+})
 
 module.exports = {
     getBlogs,
