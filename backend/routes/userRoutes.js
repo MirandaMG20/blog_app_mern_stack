521f163aa363fa533bfd4399243f8b26d62ce530
MirandaMG20 <guadamiran@gmail.com>
2023-09-08 13:45:13 -0400

    Delete User's Account

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,13 @@ const router = require('express').Router()
 const bcrypt = require('bcrypt')
 // Import User Model
 const User = require('../models/userModel')
+// Import Blog Model
+const Blog = require('../models/blogModel')
 
-// Update User
+// Update User's Information
 router.put('/:id', async (req, res) => {
 
+    // Password Hashing
     if (req.body.userId === req.params.id) {
         if (req.body.password) {
             const salt = await bcrypt.genSalt(10)
@@ -13,13 +16,15 @@ router.put('/:id', async (req, res) => {
         }
 
         try {
+            // Update User in the Database
             const updateUser = await User.findByIdAndUpdate(
                 req.params.id,
                 {
+                    // $set: req.body = MongoDB will update only the specified fields (name, email, and password) with their new values, leaving other fields (e.g., timestamps or any other fields not present in req.body) unchanged.
                     $set: req.body,
                 },
                 {
-                    new: true, 
+                    new: true,
                 }
             )
             res.status(200).json(updateUser)
@@ -28,6 +33,30 @@ router.put('/:id', async (req, res) => {
         }
     } else {
         res.status(401).json('Update your account')
+    }
+})
+
+// Delete User's Account
+router.delete('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        // delete user's account and blogs
+
+        try {
+            const user = await User.findById(req.params.id)
+            try {
+                await Blog.deleteMany({ name: user.name })
+
+                // only delete user account
+                await User.findByIdAndRemove(req.params.id)
+                res.status(200).json('User has been deleted.')
+            } catch (error) {
+                res.status(500).json(error)
+            }
+        } catch (error) {
+            res.status(404).json('User not found...')
+        }
+    } else {
+        res.status(401).json('You are not authorized to delete this account.')
     }
 })
 
