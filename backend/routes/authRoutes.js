ca8daa7b9cecb1d70d2541e53d73eb58ec90b6ad
MirandaMG20 <guadamiran@gmail.com>
2023-09-08 12:04:55 -0400

    User Login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,23 @@
+const asyncHandler = require('express-async-handler')
 const router = require('express').Router()
-const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
+// Import User Model
+const User = require('../models/userModel')
 
-// New User Register
+
+//@desc     Register new user
+//@route    POST /auth/register
+//@access   Public
 router.post('/register', async (req, res) => {
     try {
-        const salt = await bcrypt.genSalt(8)
-        const hashedPass = await bcrypt.hash(req.body.password, salt)
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
+        // Create User
         const newUser = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         })
 
         const user = await newUser.save()
@@ -21,28 +27,30 @@ router.post('/register', async (req, res) => {
     }
 })
 
-
-// Login
+//@desc   Authenticate a user
+//@route  POST /auth/login
+//@access Public
 router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({ name: req.body.name })
+        const { email, password } = req.body
+
+        const user = await User.findOne({ email })
 
         // If no user
         if (!user) {
-           return res.status(400).json('No user found')
+            return res.status(400).json('No user found')
         }
 
         // If the user exists, compare the password
-        const validate = await bcrypt.compare(req.body.password, user.password)
-
-        // If the password is not valid
-        if (!validate) {
-            return res.status(400).json('Wrong login!');
+        if (await bcrypt.compare(password, user.password)) {
+            res.json({
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+            })
+        } else {
+            res.status(400).json('Wrong login!')
         }
-
-        // If everything is valid, send the user data (excluding password)
-        const { password, ...other } = user._doc
-        return res.status(200).json(other)
     } catch (error) {
         return res.status(500).json(error)
     }
