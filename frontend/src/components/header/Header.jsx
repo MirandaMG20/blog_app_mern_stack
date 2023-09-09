56df379ad8d5c742ec0c5a46ad014088517eb82f
MirandaMG20 <guadamiran@gmail.com>
2023-09-09 10:14:59 -0400

    CSS

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from '../../../public/images/wide-miranda-logo-transparent.png'
 import { Link } from "react-router-dom"
 
 function Header() {
@@ -9,7 +8,7 @@ function Header() {
                 <div className=''>
                     <div className='logo'>
                        
-                        <img src={logo} alt='logo' width='300px' />
+                        <img src='images/wide-miranda-logo-transparent.png' alt='logo' width='300px' />
                         
                     </div>
                 </div>
