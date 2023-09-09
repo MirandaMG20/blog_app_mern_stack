d888086d750a569dc4612cfb88bdf91e04a3655e
MirandaMG20 <guadamiran@gmail.com>
2023-09-09 12:41:53 -0400

    Header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from "react-router-dom"
+import { nav } from '../../data/data'
 
 function Header() {
     return (
         <>
+
             <header className='header'>
-                <div className=''>
-                    <div className='logo'>
-                       
-                        <img src='images/wide-miranda-logo-transparent.png' alt='logo' width='300px' />
-                        
-                    </div>
+
+                <div>
+                    <a href="">HOME</a>
+                    {/* <ul>
+                        {nav.map((link) => (
+                            <li key={link.id}>
+                                <Link to={link.url}>{link.text}</Link>
+                            </li>
+                        ))}
+                    </ul> */}
                 </div>
+
+                <div className='logo'>
+                    <img src='images/wide-miranda-logo-transparent.png' alt='logo' width='300px' />
+                </div>
+
+                <div className="buttons">
+                    <Link to='/register'>
+                        <FaUser /> Register
+                    </Link>
+                    <br />
+                    <Link to='/login'>
+                        <FaSignInAlt /> Login
+                    </Link>
+                    <br />
+                    <Link >
+                        <FaSignOutAlt /> Logout
+                    </Link>
+                </div>
+
             </header>
         </>
     )
