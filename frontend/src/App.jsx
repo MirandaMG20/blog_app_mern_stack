1f880f5548a4c3cd15c5b5f2b00d094479145110
MirandaMG20 <guadamiran@gmail.com>
2023-09-08 22:00:24 -0400

    Frontend First Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import React from "react"
+import Header from "./components/header/Header"
 
 function App() {
 
   return (
     <>
+      <Header />
       <h1>Blog App</h1>
     </>
   )
 }
+
 
 export default App
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { Header } from '../../components/header/Header'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className=''>
+                    <div className=''>
+                        <img src="" alt='logo' width='100px' />
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
